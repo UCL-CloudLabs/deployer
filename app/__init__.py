e23af3d328f0c52c7051d15984066d0e349c0cd6
Raquel Alegre <r.alegre@ucl.ac.uk>
2017-07-03 17:54:28 +0100

    Propagates VM name given by user in initial form.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from .forms.DeployForm import DeployForm
 from .deployer.deployer import Deployer
 
@@ -8,12 +8,14 @@ app = Flask(__name__)
 # Configure Flask app
 app.config.from_object('config')
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def show_deploy_options():
     '''
     Present form for user to fill in with information to deploy a VM on Azure.
     '''
     form = DeployForm()
+    if form.validate_on_submit():
+        return redirect(url_for('deploy_vm', vm_name=form.vm_name.data))
     return render_template('deploy_options.html', form=form)
 
 @app.route('/deploy/<vm_name>')
