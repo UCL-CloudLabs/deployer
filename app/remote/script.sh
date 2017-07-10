8f4ba63f6a4de5850cdace694d1385a3843c23a5
Raquel Alegre <r.alegre@ucl.ac.uk>
2017-07-10 16:20:14 +0100

    Delete folders only used by the Azure Python SDK deployment.

--- app/remote/script.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install docker.io -y
-git clone https://github.com/UCL-CloudLabs/Docker-sample.git
-cd Docker-sample
-sudo docker build -t hello-flask .
-sudo docker run -p 5000:5000 hello-flask
