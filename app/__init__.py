b5bb440a6d482f41f318a434ee4b84f3c7a6c211
Raquel Alegre <r.alegre@ucl.ac.uk>
2017-07-11 15:55:05 +0100

    Import correction.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from .forms.DeployForm import DeployForm
+from .forms.deploy_form import DeployForm
 from .deployer.deployer import Deployer
 
 # Create Flask app
