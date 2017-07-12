0e11fc0d3d4b26c29df67a6939a7d869ec26f0f4
Raquel Alegre <r.alegre@ucl.ac.uk>
2017-07-12 17:31:14 +0100

    Testing Travis setup.

--- test/conftest.py
+++
@@ -1,6 +0,0 @@
-from app import create_app
-
-@pytest.fixture
-def app():
-    app = create_app()
-    return app
---
+++ test/test_deployer.py
@@ -0,0 +1,5 @@
+from app.deployer.deployer import Deployer
+
+class TestDeployer:
+    def test_test(self):
+        assert 1==1
