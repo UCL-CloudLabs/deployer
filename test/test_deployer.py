f302ccc9ab7401f72e8d67408d2ec1d8b54b4e81
Raquel Alegre <r.alegre@ucl.ac.uk>
2017-07-13 18:08:29 +0100

    Create deployer as part of tests.

--- test/test_deployer.py
+++ test/test_deployer.py
@@ -1,6 +1,13 @@
 from app.deployer.deployer import Deployer
+from pathlib import Path
 
 
 class TestDeployer:
-    def test_test(self):
-        assert 1 == 1
+    def setup_method(self):
+        self.d = Deployer()
+
+    def test_deployer1(self):
+        assert self.d.tf_path == Path('app/deployer/terraform').absolute()
+
+    def teardown_method(self):
+        pass
