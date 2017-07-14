45ee5974e160abd3c45b4f76c00dc24c937d5c40
Raquel Alegre <r.alegre@ucl.ac.uk>
2017-07-14 12:39:27 +0100

    Added detroy resource as part of tear down test method.

--- test/test_deployer.py
+++ test/test_deployer.py
@@ -1,13 +1,35 @@
 from app.deployer.deployer import Deployer
-from pathlib import Path
+from app.deployer.host import Host
+from pathlib import Path, PosixPath
+from haikunator import Haikunator
 
 
 class TestDeployer:
     def setup_method(self):
-        self.d = Deployer(Path('../app').absolute())
+        self.d = Deployer(Path('app').absolute())
+        haiku = Haikunator()
+        self.resource_name = haiku.haikunate()
 
-    def test_deployer1(self):
+    def test_deployer_config(self):
         assert self.d.tf_path == Path('app/deployer/terraform').absolute()
 
+    # def test_deployer(self):
+    #     host = self._create_host()
+    #     self.d.deploy(self.host)
+    #     # ping machine? Get URL from state.
+    #     assert self.d.tf_path == Path('app/deployer/terraform').absolute()
+
     def teardown_method(self):
-        pass
+        self.d.destroy(self.resource_name)
+
+    def _create_host(self):
+        haiku = Haikunator()
+        dnsname = haiku.haikunate(delimiter='', token_length=10)
+        username = 'testuser'
+        passwd = 'Password123'
+        public_key = 'ssh-rsa aaaaaa email@host.com'
+        private_key_path = '~/.ssh/id_rsa'
+        self.host = Host(name=self.resource_name, dnsname=dnsname,
+                         username=username, passwd=passwd,
+                         public_key=public_key,
+                         private_key_path=private_key_path)
