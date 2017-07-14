1867a60008073a648ae4f7e6dcdbc8d870f312b6
Raquel Alegre <r.alegre@ucl.ac.uk>
2017-07-14 14:55:27 +0100

    Use Haikunator to create randome names or Azure resources in the tests.

--- test/test_deployer.py
+++ test/test_deployer.py
@@ -8,28 +8,27 @@ class TestDeployer:
     def setup_method(self):
         self.d = Deployer(Path('app').absolute())
         haiku = Haikunator()
-        self.resource_name = haiku.haikunate()
+        self.resource_name = haiku.haikunate(delimiter='', token_length=4)
+        self.dnsname = haiku.haikunate(delimiter='', token_length=4)
 
     def test_deployer_config(self):
         assert self.d.tf_path == Path('app/deployer/terraform').absolute()
 
-    # def test_deployer(self):
-    #     host = self._create_host()
-    #     self.d.deploy(self.host)
-    #     # ping machine? Get URL from state.
-    #     assert self.d.tf_path == Path('app/deployer/terraform').absolute()
+    def test_deployer(self):
+        host = self._create_host()
+        self.d.deploy(self.host)
+        # ping machine? Get URL from state.
+        assert self.d.tf_path == Path('app/deployer/terraform').absolute()
 
     def teardown_method(self):
         self.d.destroy(self.resource_name)
 
     def _create_host(self):
-        haiku = Haikunator()
-        dnsname = haiku.haikunate(delimiter='', token_length=10)
         username = 'testuser'
         passwd = 'Password123'
         public_key = 'ssh-rsa aaaaaa email@host.com'
         private_key_path = '~/.ssh/id_rsa'
-        self.host = Host(name=self.resource_name, dnsname=dnsname,
+        self.host = Host(name=self.resource_name, dnsname=self.dnsname,
                          username=username, passwd=passwd,
                          public_key=public_key,
                          private_key_path=private_key_path)
